f50f9f12a09edbd749b6fe68d76e82bed0d82684
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2020-12-18 07:36:41 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,48 @@ class TasksController < ApplicationController
   end
   
   def new
+    @task = Task.new
   end
   
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクを作成しました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクを作成出来ませんでした"
+      render :new
+    end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクを編集しました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクを編集出来ませんでした"
+      render :edit
+    end
   end
   
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクを削除しました"
+    # redirect_to tasks_url
+    redirect_to tasks_url
+  end
+  
+  public
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
