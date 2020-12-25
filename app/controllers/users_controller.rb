d3b40d34532bffa3a8d52206e9bba65f3a5a9f4e
takumi.nakashima <nakashima.2004.abenomix@gmail.com>
2020-12-25 14:53:46 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
+      # redirect_to @user
       redirect_to @user
     else
       flash[:danger] = 'ユーザの登録に失敗しました。'
